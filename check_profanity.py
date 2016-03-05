1d43b1af7d3943f68a063281a853d16dc496b3de
Gurpreet Singh <gdhuper@gmail.com>
2016-03-04 17:42:15 -0800

    New file added

--- check_profanity.py
+++ check_profanity.py
@@ -1,8 +1,17 @@
-import turtle
+import urllib
 
 def read_text():
-    text = open("C:\Users\gopi\Documents\Python\sampletext.txt")
-    print(text.read())
-    text.close()   
+    text = open("C:\Users\gopi\Documents\Python\sampleText.txt")
+    contents = text.read()
+    print(contents)
+    text.close()
+    check_profanity(contents)
+
+def check_profanity(text_to_check):
+    connection = urllib.urlopen("http://www.wdyl.com/profanity?q="+text_to_check)
+    output = connection.read()
+    print(output)
+    connection.close()
+    
 
 read_text()
