02064ec214b98c02665ef437321dd85782c8631f
Gurpreet Singh <gdhuper@gmail.com>
2016-03-04 17:59:40 -0800

    Add Pirate_speech method

--- check_profanity.py
+++ check_profanity.py
@@ -4,8 +4,13 @@ def read_text():
     text = open("C:\Users\gopi\Documents\Python\sampleText.txt")
     contents = text.read()
     print(contents)
+    print("")
     text.close()
+    print("Does the document contains any profane Words?")
     check_profanity(contents)
+    print("")
+    print("This is how a pirate will say the same text")
+    pirate_speech(contents)
 
 def check_profanity(text_to_check):
     connection = urllib.urlopen("http://www.wdyl.com/profanity?q="+text_to_check)
@@ -18,6 +23,11 @@ def check_profanity(text_to_check):
         print("No curse Words! Yayyy Its clean as sh*t")
     else:
         print("Could not scan the document")
+def pirate_speech(text_to_convert):
+    c = urllib.urlopen(" http://isithackday.com/arrpi.php?text=" + text_to_convert)
+    output = c.read()
+    print(output)
+    c.close()
     
 
 read_text()
