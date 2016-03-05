2e10188622f436bbe80e3c2e1a9f8a6456244a96
Gurpreet Singh <gdhuper@gmail.com>
2016-03-04 17:47:18 -0800

    Add conditional statements for print method

--- check_profanity.py
+++ check_profanity.py
@@ -10,8 +10,14 @@ def read_text():
 def check_profanity(text_to_check):
     connection = urllib.urlopen("http://www.wdyl.com/profanity?q="+text_to_check)
     output = connection.read()
-    print(output)
+    #print(output)
     connection.close()
+    if "true" in output:
+        print("Profanity Alert")
+    elif "false" in output:
+        print("No curse Words! Yayyy Its clean as sh*t")
+    else:
+        print("Could not scan the document")
     
 
 read_text()
